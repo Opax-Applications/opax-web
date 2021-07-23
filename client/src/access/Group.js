cb1986a239e6cbbd315c69885d4621fe56628784
Jake Tracey <jaketracey@gmail.com>
2021-07-23 16:03:33 +1000

    added group stuff

--- client/src/access/Group.js
+++ client/src/access/Group.js
@@ -1,10 +1,42 @@
 import React, { Component } from 'react';
 
-import Alert from 'react-bootstrap/Alert';
-import Breadcrumb from 'react-bootstrap/Breadcrumb';
-import Button from 'react-bootstrap/Button';
+import { Box, Alert, Flex, Heading, SimpleGrid, Breadcrumb, BreadcrumbItem, BreadcrumbLink, Text, Input, Checkbox, NumberInput, PinInput, Radio, Select, Slider, Switch, Textarea, Button, theme } from '@chakra-ui/react';
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+} from '@chakra-ui/react';
+import { Stack, HStack, VStack } from "@chakra-ui/react";
+import {
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  FormHelperText,
+} from "@chakra-ui/react";
+import {
+  Stat,
+  StatLabel,
+  StatNumber,
+  StatHelpText,
+  StatArrow,
+  StatGroup,
+} from '@chakra-ui/react';
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+} from "@chakra-ui/react";
+
 import Form from 'react-bootstrap/Form';
-import Table from 'react-bootstrap/Table';
 
 import { faTrashAlt } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -70,7 +102,8 @@ class Group extends Component {
   
   componentDidUpdate(prevProps, prevState) {
     if ((this.state.error && !prevState.error) || (this.state.success && !prevState.success))
-      document.querySelector('.alert').focus();
+      // document.querySelector('.alert').focus();
+      return;
   }
   
   updateUsersToAdd() {
@@ -180,13 +213,15 @@ class Group extends Component {
   breadcrumbs() {
     return (
       <Breadcrumb>
-        <LinkContainer to="/">
-          <Breadcrumb.Item>Home</Breadcrumb.Item>
-        </LinkContainer>
-        <LinkContainer to="/groups">
-          <Breadcrumb.Item>Groups</Breadcrumb.Item>
-        </LinkContainer>
-        <Breadcrumb.Item active>Group</Breadcrumb.Item>
+        <BreadcrumbItem>
+          <BreadcrumbLink href="/">Home</BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbItem>
+          <BreadcrumbLink href={'/groups/'}>Groups</BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbItem>
+          <BreadcrumbLink current href={'#'}>{this.state.group.name}</BreadcrumbLink>
+        </BreadcrumbItem>
       </Breadcrumb>
     );
   }
@@ -196,15 +231,15 @@ class Group extends Component {
         this.state.group.name === 'Authenticated')
       return null;
     return this.state.group.users.map(user => (
-      <tr key={user._id}>
-        <td className="code"><Link to={'/users/'+user._id}>{user.username}</Link></td>
-        <td className="text-right">
+      <Tr key={user._id}>
+        <Td><Link to={'/users/'+user._id}>{user.username}</Link></Td>
+        <Td>
           <Button title="Remove" variant="danger" size="xs"
               onClick={(e) => this.removeUser(user._id)}>
             <FontAwesomeIcon icon={faTrashAlt} title="Remove" />
           </Button>
-        </td>
-      </tr>
+        </Td>
+      </Tr>
     ));
   }
   
@@ -261,7 +296,7 @@ class Group extends Component {
         <>
           {this.breadcrumbs()}
           <h1>Group</h1>
-          <Alert variant="danger">
+          <Alert mb="5" color={"#444"} status="info" variant="subtle">
             You are not allowed to edit groups.
           </Alert>
         </>
@@ -270,133 +305,152 @@ class Group extends Component {
     const usersHTML = this.groupUserList();
     return (
       <>
-        {this.breadcrumbs()}
-        <h1>{this.state.group._id ? "Group: " + this.state.group.name : "New Group"}</h1>
-        <Alert show={this.state.error != null} variant="danger" dismissible
+        <Flex direction="column" h="100vh">
+          <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
+
+            <Box w="100%" mb="4">
+              {this.breadcrumbs()}
+              <Text mt="5" mb="5" isTruncated fontSize="5xl">{this.state.group._id ? "Group: " + this.state.group.name : "New Group"}</Text>
+
+              <Alert mb="5" color={"#fff"} status="warning" 
+ show={this.state.error != null} variant="danger" dismissible
             onClose={() => this.setState({ error: null })} tabIndex="0">
-          {this.state.error}
-        </Alert>
-        <Alert show={this.state.success != null} variant="success" dismissible
+                {this.state.error}
+              </Alert>
+              <Alert show={this.state.success != null} variant="success" dismissible
             onClose={() => this.setState({ success: null })} tabIndex="0">
-          {this.state.success}
-        </Alert>
-        {this.state.group._id != null &&
-          <Button title="Remove" variant="danger" size="sm" onClick={(e) => this.removeGroup()}>Remove group</Button>
-        }
-        <Form onSubmit={e => { e.preventDefault(); this.saveGroup(); } } className="form mt-3 border">
-          <Form.Group controlId="name">
-            <Form.Label>Group Name</Form.Label>
-            <Form.Control name="name" type="text" size="30"
+                {this.state.success}
+              </Alert>
+              
+              <Form onSubmit={e => { e.preventDefault(); this.saveGroup(); } } className="form mt-3 border">
+                <Stack spacing={5}>
+                  <FormControl id="name">
+                    <FormLabel column sm="5">Group Name</FormLabel>
+                    <Input name="name" type="text"
               value={this.state.group.name} style={{ width:'auto' }}
               required onChange={e => this.handleGroupChange(e)}/>
-          </Form.Group>
-          <Form.Group controlId="createAllAudits">
-            <Form.Check name="createAllAudits" type="checkbox"
+                  </FormControl>
+                  <FormControl id="createAllAudits">
+                    <Checkbox name="createAllAudits" type="checkbox"
               value={this.state.group.permissions.createAllAudits}
               onChange={e => this.handlePermissionChange(e)}
-              checked={this.state.group.permissions.createAllAudits}
-              label="Create any audit"/>
-          </Form.Group>
-          <Form.Group controlId="readAllAudits">
-            <Form.Check name="readAllAudits" type="checkbox"
+              checked={this.state.group.permissions.createAllAudits}>Create any audit</Checkbox>
+                  </FormControl>
+                  <FormControl id="readAllAudits">
+                    <Checkbox name="readAllAudits" type="checkbox"
               value={this.state.group.permissions.readAllAudits}
               onChange={e => this.handlePermissionChange(e)}
               checked={this.state.group.permissions.readAllAudits}
-              label="Read all audits"/>
-          </Form.Group>
-          <Form.Group controlId="deleteAllAudits">
-            <Form.Check name="deleteAllAudits" type="checkbox"
+                    >Read all audits</Checkbox>
+                  </FormControl>
+                  <FormControl id="deleteAllAudits">
+                    <Checkbox name="deleteAllAudits" type="checkbox"
               value={this.state.group.permissions.deleteAllAudits}
               onChange={e => this.handlePermissionChange(e)}
               checked={this.state.group.permissions.deleteAllAudits}
               disabled={this.state.group.name === 'Guests'}
-              label="Delete all audits"/>
-          </Form.Group>
-          <Form.Group controlId="editUsersAndGroups">
-            <Form.Check name="editUsersAndGroups" type="checkbox"
+                    >Delete all audits</Checkbox>
+                  </FormControl>
+                  <FormControl id="editUsersAndGroups">
+                    <Checkbox name="editUsersAndGroups" type="checkbox"
               value={this.state.group.permissions.editUsersAndGroups}
               onChange={e => this.handlePermissionChange(e)}
               checked={this.state.group.permissions.editUsersAndGroups}
               disabled={this.state.group.name === 'Guests'}
-              label="Edit users and groups"/>
-          </Form.Group>
-          <section className="mt-4">
-            <h2>Domains</h2>
-            {this.state.group.permissions.domains.length > 0 ?
-              <Table id="domains" bordered size="sm" className="data">
-                <thead>
-                  <tr><th id="dname">Domain Name</th><th>Create</th><th>Read</th><th>Delete</th><th></th></tr>
-                </thead>
-                <tbody>
-                  {this.state.group.permissions.domains.map((d, index) =>
-                    <tr key={index}>
-                      <td><Form.Control id={`domain_${index}_name`} type="text"
-                        size="30" value={d.name} required aria-labelledby="dname"
-                        onChange={e => this.handleDomainChange(e)}/></td>
-                      <td><Form.Check id={`domain_${index}_create`} type="checkbox"
+                    >Edit users and groups</Checkbox>
+                  </FormControl>
+                  <section>
+                    <Text fontSize="3xl" mt="5" mb="0">Domains</Text>
+                    {this.state.group.permissions.domains.length > 0 ?
+                      <Table mt="5" id="domains" colorScheme="whiteAlpha" variant="simple">
+                        <Thead>
+                          <Tr><Th id="dname">Domain Name</Th><th>Create</th><th>Read</th><th>Delete</th><th></th></Tr>
+                        </Thead>
+                        <Tbody>
+                          {this.state.group.permissions.domains.map((d, index) =>
+                            <Tr key={index}>
+                              <Td>
+                                <FormControl>
+
+                                  <Input id={`domain_${index}_name`} type="text"
+                         value={d.name} required aria-labelledby="dname"
+                        onChange={e => this.handleDomainChange(e)}/></FormControl>
+                              </Td>
+                              <Td><Checkbox id={`domain_${index}_create`} type="checkbox"
                         value={d.create} onChange={e => this.handleDomainChange(e)}
-                        checked={d.create} label="Create"/></td>
-                      <td><Form.Check id={`domain_${index}_read`} type="checkbox"
+                        checked={d.create} label="Create"/></Td>
+                              <Td><Checkbox id={`domain_${index}_read`} type="checkbox"
                         value={d.read} onChange={e => this.handleDomainChange(e)}
-                        checked={d.read} label="Read"/></td>
-                      <td><Form.Check id={`domain_${index}_delete`} type="checkbox"
+                        checked={d.read} label="Read"/></Td>
+                              <Td><Checkbox id={`domain_${index}_delete`} type="checkbox"
                         value={d.delete} onChange={e => this.handleDomainChange(e)}
-                        checked={d.delete} label="Delete"/></td>
-                      <td><Button title="Remove" variant="danger" size="xs"
+                        checked={d.delete} label="Delete"/></Td>
+                              <Td><Button title="Remove" variant="danger" size="xs"
                         onClick={(e) => this.removeDomain(index)}>
-                        <FontAwesomeIcon icon={faTrashAlt} title="Remove" />
-                      </Button></td>
-                    </tr>
-                  )}
-                </tbody>
-              </Table>
-              :
-              <p>No domain-specific rule.</p>
-            }
-            <Button variant="secondary" size="sm" className="ml-2"
+                                <FontAwesomeIcon icon={faTrashAlt} title="Remove" />
+                              </Button></Td>
+                            </Tr>
+                          )}
+                        </Tbody>
+                      </Table>
+                      :
+                      <p>No domain-specific rule.</p>
+                    }
+                    <Button colorScheme="pink"
+          size="lg" mt="5" mb="5"
                 onClick={e => this.addDomain()}>
               Add a domain
-            </Button>
-          </section>
-          <div className="text-center">
-            <Button variant="primary" type="submit">
+                    </Button>
+                  </section>
+                  <div className="text-center">
+                    <Button Button mb="5" mr="5" mt="5" colorScheme="pink"
+          size="lg" type="submit">
               Save
-            </Button>
-          </div>
-        </Form>
-        {usersHTML &&
+                    </Button>
+                    {this.state.group._id != null &&
+          <Button mb="5" mt="5" colorScheme="blue"
+          size="md" onClick={(e) => this.removeGroup()}>Remove group</Button>
+                    }
+                  </div>
+                </Stack>
+              </Form>
+              {usersHTML &&
           <section className="border">
-            <h2>Users</h2>
-            <Table bordered size="sm" className="data">
-              <thead>
-                <tr>
-                  <th>Name</th>
-                  <th className="text-right"></th>
-                </tr>
-              </thead>
-              <tbody>
+            <Text mt="5" mb="5" isTruncated fontSize="4xl">Users in this group</Text>
+            <Table colorScheme="whiteAlpha" variant="simple">
+              <Thead>
+                <Tr>
+                  <Th>Name</Th>
+                  <Th></Th>
+                </Tr>
+              </Thead>
+              <Tbody>
                 {usersHTML}
-              </tbody>
+              </Tbody>
             </Table>
             {this.state.usersToAdd && this.state.usersToAdd.length > 0 &&
               <Form onSubmit={e => { e.preventDefault(); this.addUser(); } } inline>
-                <Form.Group controlId="selectedUser">
-                  <Form.Label className="mr-1">Add a user:</Form.Label>
-                  <Form.Control name="selectedUser" as="select" value={this.state.selectedUser}
+                <FormControl id="selectedUser">
+                  <FormLabel className="mr-1">Add a user:</FormLabel>
+                  <Input name="selectedUser" as="select" value={this.state.selectedUser}
                       onChange={e => this.handleChange(e)}>
                     {this.state.usersToAdd
                       .map((u) =>
                         <option key={u._id} value={u._id}>{u.username}</option>
                       )}
-                  </Form.Control>
-                </Form.Group>
-                <Button variant="primary" type="submit" className="ml-2">
+                  </Input>
+                </FormControl>
+                <Button colorScheme="pink"
+          size="lg" mt="5" mb="5">
                   Add
                 </Button>
               </Form>
             }
           </section>
-        }
+              }
+            </Box>
+          </Flex>
+        </Flex>
       </>
     );
   }
